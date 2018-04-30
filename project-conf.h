95bb6ee920fd601861d8406862c4d211ac3b2f30
Bhaumik Bhandari <bhaumik.bhandari@altiux.com>
2018-04-30 15:20:10 +0530

    Added Project Conf

---
+++ project-conf.h
@@ -0,0 +1,8 @@
+#ifndef __MV_CLIENT_TEST_CONF_H__
+#define __MV_CLIENT_TEST_CONF_H__
+/*---------------------------------------------------------------------------*/
+#ifndef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC             nullrdc_driver
+#endif
+/*---------------------------------------------------------------------------*/
+#endif /* __MV_CLIENT_TEST_CONF_H__ */
